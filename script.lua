ecabf26c5da869978de9831d18589487f8474d9f
PiggyWhiskey <bromy2004@gmail.com>
2019-04-11 10:57:04 +0930

    aa

--- script.lua
+++ script.lua
@@ -10,16 +10,16 @@ function find_Coastline(debug)
     -- -1, -1 = North West
     local max_Search = -100
     local chunkBound
-    local groundCount
+    local ground_Count
     
     
     for iChunk = -10, max_Search, -1
     do
         chunkBound = get_Chunk_BoundingBox(0, iChunk)
         --It's either water-tile or ground-tile, doesn't require both (Unless Void?)
-        groundCount = get_Ground_Count(chunkBound)
+        ground_Count = get_Ground_Count(chunkBound)
 
-        if groundCount < 512 then
+        if ground_Count > 0 and ground_Count < 512 then
             --Less than 50% Ground, Valid Chunk
             chart_Coastline(0, ichunk)
             iChunk = max_Search
@@ -30,17 +30,25 @@ end
 
 function chart_Coastline(x, y)
     local player = game.forces.player
+    local ground_Count
 
     --Chart 3x3
     for i = -1, 1, 1
     do
         for j = -1, 1, 1
         do
-            )
-            chart_Chunk(player, game.player.surface, x + i , y + i )
+
+            --Reveal Chunk
+            chart_Chunk(player, game.player.surface, x + i , y + j )
+
+            --Count ground-tile
+            ground_Count get_Ground_Count(get_Chunk_BoundingBox( x + i, y + j ))
+
+            --Ground between 0% and 50%, recurse
+            if ground_Count > 0 and ground_Count < 512 then
+                chart_Coastline(x + i, y + j)
+                    
+            end
         end
     end
-
-
-
 end
